6e78349f9296de7b56b7eaf2adf6d91a281a3ccb
lorna <lornamuringi@gmail.com>
2026-02-27 13:39:02 +0300

    Added a styling theme in Tailwind

--- Quiz-app/tailwind.config.js
+++ Quiz-app/tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary": "#cbeceb",
+        "secondary": "#cd8ee4",
+        "accent": "#2c55e0",
+        "background": "#F0F8FF",
+        "text": "#333333",
+      },
+    },
   },
   plugins: [],
 }
