0a1a01361dd19b98353e647106dd2811238e6dca
lorna <lornamuringi@gmail.com>
2026-02-28 19:52:59 +0300

    Implement basic question card with local scoring

--- Quiz-app/src/App.jsx
+++ Quiz-app/src/App.jsx
@@ -1,18 +1,22 @@
-import {  Routes, Route } from "react-router-dom";
 import QuizStart from "./components/QuizStart";
-
-
+import { useState } from "react";
 
 function App() {
     const [started, setStarted] = useState(false);
-
+    const questions = [
+  {
+    question: "What is 2 + 2?",
+    correct_answer: "4",
+    incorrect_answers: ["3", "5", "6"],
+  },
+];
   
   return (
     <div className="min-h-screen flex items-center justify-center p-4 bg-gray-100">
        {!started ? (
         <QuizStart onStart={() => setStarted(true)} />
       ) : (
-        <p>Quiz goes here...</p>
+        <p>Quiz questions go here..</p>
       )}
     </div>
   );
--- Quiz-app/src/components/QuestionCard.jsx
+++ Quiz-app/src/components/QuestionCard.jsx
@@ -1,19 +1,45 @@
 import { useQuizStore } from "../store/quizStore";
+import { useState } from "react";
 
-function QuizPage() {
-  const { questions, currentIndex, score, answerQuestion } =
-    useQuizStore();
+function  QuestionCard({ question, onFinish }) {
 
-  const currentQuestion = questions[currentIndex];
+  const [score, setScore] = useState(0); 
+  const [answered, setAnswered] = useState(false);
+
+  const handleAnswer = (answer) => {
+    if (answer === question.correct_answer) {
+      setScore(score + 1);
+    }
+    setAnswered(true);
+  };
+
+    if (answered) {
+      return (
+        <div>
+        <p>Score: {score}</p>
+        <button onClick={onFinish}>Finish</button>
+      </div>
+    );
+  }
+
 
   return (
     <div>
-      <h2>{currentQuestion?.question}</h2>
-      <button onClick={() => answerQuestion(true)}>Correct</button>
-      <button onClick={() => answerQuestion(false)}>Wrong</button>
-      <p>Score: {score}</p>
+      <h2>{question.question}</h2>
+      {[question.correct_answer, ...question.incorrect_answers].map(
+        (ans, index) => (
+          <button
+            key={index}
+            onClick={() => handleAnswer(ans)}
+            className="block bg-primary text-white p-2 m-2 rounded hover:bg-blue-600"
+          >
+            {ans}
+          </button>
+        )
+      )}
     </div>
   );
 }
 
-export default QuizPage;
+
+export default  QuestionCar;
